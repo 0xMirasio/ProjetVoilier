32e121e91a520bc36e6701b70aad39383cd80709
iattach <memoryinwinter@outlook.com>
2020-11-20 18:25:23 +0100

    Update commentaire de Accéléromètre.h

--- MyDrivers/Accelerometre.h
+++ MyDrivers/Accelerometre.h
@@ -4,12 +4,17 @@
 #include "stm32f1xx_ll_adc.h"
 #include "stm32f1xx_ll_rcc.h"
 
+//configurer ADC
 void Adc_Conf_ACC(ADC_TypeDef * adc);
 
+//obtenir angle
 float getAngle(void);
 
+//manipuler le servo-moteur selon l'angle
 void bougerVoile(float angle);
 
+//initialiser TIM pour servo-moteur
 void timer_pwm_init(void);
 
+//initialiser GPIO pour servo-moteur
 void gpio_servom_init(void);
