6796beb76b5a3ea61e9f5b255f18438b97b0f8e2
Youssef-prog <58248934+Youssef-prog@users.noreply.github.com>
2020-11-20 18:16:37 +0100

    Update Girouette.h

--- MyDrivers/Girouette.h
+++ MyDrivers/Girouette.h
@@ -4,9 +4,9 @@
 #include "stm32f1xx_ll_bus.h" // Pour l'activation des horloges
 #include "stm32f1xx_ll_tim.h" 
 
-float girouette_get_angle(void); //Récypère l'angle de la girouette
+float girouette_get_angle(void); //RÃ©cupÃ¨re l'angle de la girouette
 void girouetteConf(void);//Configure le Timer et le GPIO
-void EXTI9_5_IRQnHandler(void);//Avertit pour le zéro absolu lors d'un tour complet
+void EXTI9_5_IRQnHandler(void);//Avertit pour le zÃ©ro absolu lors d'un tour complet
 void startTimer(TIM_TypeDef* TIMx);//Active le compteur
 void stopTimer(TIM_TypeDef* TIMx);//Stop le compteur
 
